4a4296c9237e61336afa2c0d5e13bf4b08df6092
vicky <vicky.syafira94@gmail.com>
2023-06-03 22:02:09 +0700

    edit by vicky

--- ecomm/ecomm/settings.py
+++ ecomm/ecomm/settings.py
@@ -99,23 +99,23 @@ WSGI_APPLICATION = 'ecomm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'database_project',
-        'USER': 'postgres',
-        'PASSWORD': 'momonska',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+     'default': {
+     'ENGINE': 'django.db.backends.sqlite3',
+     'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'database_project',
+#        'USER': 'postgres',
+#        'PASSWORD': 'momonska',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+#}
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
